5f5fac7d7c97cc4b648da1d4684c3ecd567b9551
Diego S Oliveira <diego-siebra@hotmail.com>
2020-07-30 08:46:09 -0300

    commit inicial

---
+++ android/app/src/main/java/com/example/frase_dia/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.frase_dia;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: 'frases do dia',
+    home: Container(color: Color.fromRGBO(255, 255, 255, 0.8),),
+  ));
+}
